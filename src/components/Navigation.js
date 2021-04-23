e601f8185cd3bf83937a396bb7be4e8edf3962ad
William Ford <wford1.home@gmail.com>
2021-04-23 22:14:09 +0100

    Added cart icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,21 @@
 import React from "react";
 import "../styles/Navigation.css";
 import { Link } from "react-router-dom";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const Navigation = () => {
-
-  const menuStyle = {
-    color: "white",
-    underline: "none",
-  };
 
   return (
     <nav className="menu">
       <h3>Logo</h3>
       <ul className="menu-links">
-            <Link style={menuStyle} to="/">
+            <Link className="menuStyle" to="/">
                     <li>Home</li>
             </Link>
-            <Link style={menuStyle} to="/shop">
+            <Link className="menuStyle" to="/shop">
                     <li>Shop</li>
             </Link>
+            <ShoppingCartIcon />
       </ul>
     </nav>
   );
